0f03e7726b144ce303d8749b79aa63cb49d990fb
Joe <goldenjm@umich.edu>
2011-02-04 14:00:16 -0500

    pull

--- DBtest.php
+++ DBtest.php
@@ -1,5 +1,8 @@
 <?php
 
 require_once "db.php";
-$output = $db->getAll("SELECT * FROM test");
+$output = $db->getAll("SELECT * FROM test"); //returns an array of associative arrays with items like $output[0]['name']
+//$output = $db->query("INSERT INTO ...");
+//$output = $db->getOne("SELECT name FROM table WHERE id=?",array($id)); //Gets a single value, ?'s get replaced by values from array (safely)
+//$output = $db->getRow("SELECT * FROM table WHERE id=2"); // Gets a single row as an associative array
 print_r($output);
