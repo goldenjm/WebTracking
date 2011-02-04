dfeececa847d6fd85ddc6aa70c9908637c03f604
Joe <goldenjm@umich.edu>
2011-02-04 13:44:20 -0500

    dbtest

--- DBtest.php
+++ DBtest.php
@@ -1,13 +1,17 @@
 <?php
 
 require_once "/home6/hungryhu/php/DB.php";
+echo ("after including db");
 require_once "/home6/hungryhu/WTpw.php";
+
+echo ($dsn);
+
 $db =& DB::connect($dsn);
 if(PEAR::isError($db)){
 	die($db->getMessage());
 }
 $db->setFetchMode(DB_FETCHMODE_ASSOC);
 
-echo ("hello... <br>");
+echo ("hello2... <br>");
 $output = $db->getAll("SELECT * FROM test");
 print_r($output);
