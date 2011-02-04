73bcf18fee57811798b89a2c658daf6060ca59c5
Joe <goldenjm@umich.edu>
2011-02-04 13:41:07 -0500

    dbtest

--- DBtest.php
+++ DBtest.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once "/home6/hungryhu/php/DB.php";
-require_once "home6/hungryhu/WTpw.php";
+require_once "/home6/hungryhu/WTpw.php";
 $db =& DB::connect($dsn);
 if(PEAR::isError($db)){
 	die($db->getMessage());
