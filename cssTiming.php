ef58cb8739dfeba2fa44197cd7a4e04129f4639a
Joe <goldenjm@umich.edu>
2011-02-08 16:14:36 -0500

    css tracking

--- cssTiming.php
+++ cssTiming.php
@@ -1,7 +1,7 @@
 <html>
 <head></head>
 <body>
-<script>
+<script type="text/javascript">
 document.styleSheets[0].insertRule('a {color: green}',0);
 document.styleSheets[0].insertRule('a:visited {color: red}',0);
 var a = document.createElement('a');
