81165eabf68f26c8486329a9f9b61bf4237db278
Joe <goldenjm@umich.edu>
2011-02-08 16:11:24 -0500

    css tracking

---
+++ cssTiming.php
@@ -0,0 +1,16 @@
+<html>
+<head></head>
+<body>
+<script>
+document.styleSheets[0].insertRule('a {color: green}',0);
+document.styleSheets[0].insertRule('a:visited {color: red}',0);
+var a = document.createElement('a');
+a.href = 'http://google.com';
+var aStyle = document.defaultView.getComputerStyle(a,"");
+if (aStyle.getPropertyValue('color') == red
+{
+	alert("visited");
+}
+</script>
+</body>
+</html>
