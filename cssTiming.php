b29159e8f178164a5a0517077a5ef3c41010b16b
Joe <goldenjm@umich.edu>
2011-02-08 16:57:37 -0500

    css tracking

--- cssTiming.php
+++ cssTiming.php
@@ -3,7 +3,7 @@ $s1 = 'rgb(1,2,3)'; //style 1
 $s2 = 'rgb(88,88,88)'; //style 2
 ?>
 <html>
-<head><style>h1 {background-color: #888888;} a {color: <? echo($s1) ?>;} a:visited{color: <? echo($s2) ?>;} </style></head>
+<head><style>h1 {background-color: #888888;} a {color: <? echo($s1) ?>; } a:visited{color: <? echo($s2) ?>;} </style></head>
 <body>
 <h1>Test</h1>
 
@@ -15,6 +15,7 @@ $s2 = 'rgb(88,88,88)'; //style 2
 <script type="text/javascript">
 var a = document.createElement('a');
 a.href = 'http://google.com';
+a.innerHTML = "Click Here!";
 var aStyle = document.defaultView.getComputedStyle(a,null).getPropertyValue('color');
 document.getElementById("results").innerHTML = aStyle;
 </script>
