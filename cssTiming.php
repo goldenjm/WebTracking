337b0d84d0571579073eaa91935e69aab5730553
Joe <goldenjm@umich.edu>
2011-02-08 16:39:35 -0500

    css tracking

--- cssTiming.php
+++ cssTiming.php
@@ -6,14 +6,15 @@ $s2 = 'rgb(88,88,88)'; //style 2
 <head><style>h1 {background-color: #888888;} a {color: <? echo($s1) ?>;} a:visited{color: <? echo($s1) ?>;} </style></head>
 <body>
 <h1>Test</h1>
+
+<a href="http://google.com" id="link1">Google</a>
+<a href="http://fakesite.com" id="link2">fakesite.com</a>
+<div id="results">hi</div>
+</body>
+</html>
 <script type="text/javascript">
 var a = document.createElement('a');
 a.href = 'http://google.com';
 var aStyle = document.defaultView.getComputedStyle(a,null).getPropertyValue('color');
 document.getElementById("results").innerHTML = aStyle;
 </script>
-<a href="http://google.com" id="link1">Google</a>
-<a href="http://fakesite.com" id="link2">fakesite.com</a>
-<div id="results">hi</div>
-</body>
-</html>
