310d395309f9ac3f869c7ef59d38b41abddbd896
Joe <goldenjm@umich.edu>
2011-02-08 16:29:03 -0500

    css tracking

--- cssTiming.php
+++ cssTiming.php
@@ -1,17 +1,17 @@
 <html>
-<head><style>h1 {background-color: #888888;}</style></head>
+<head><style>h1 {background-color: #888888;} a {color: rgb(1,2,3);} a:visited{color: rgb(88,88,88);} </style></head>
 <body>
 <h1>Test</h1>
 <script type="text/javascript">
-document.styleSheets[0].insertRule('a {color: green}',0);
-document.styleSheets[0].insertRule('a:visited {color: red}',0);
 var a = document.createElement('a');
 a.href = 'http://google.com';
-var aStyle = document.defaultView.getComputerStyle(a,"");
+var aStyle = document.defaultView.getComputedStyle('a',null);
 if (aStyle.getPropertyValue('color') == red)
 {
 	alert("visited");
 }
 </script>
+<a href="http://google.com" id="link1">Google</a>
+<a href="http://fakesite.com" id="link2">fakesite.com</a>
 </body>
 </html>
