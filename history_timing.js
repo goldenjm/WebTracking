5ad828ce1c698c939807f5af9a6bab047e47832e
unknown <Ituralde@.(none)>
2011-02-08 17:47:00 -0500

    fixt mai fail (parentheses in js)

--- history_timing.js
+++ history_timing.js
@@ -28,7 +28,7 @@ url_loader = function(text_url){
 	raw_list.open("GET", text_url, false);
 	raw_list.send(null);
 	var split_list = raw_list.split('\n');
-	for var index in split_list{
+	for (var index in split_list){
 		var temp = split_list[index].split(" ");
 		var temp_id = temp[0];
 		var temp_url = temp[1];
@@ -43,7 +43,7 @@ document.onReady = function(){
 	var loader = url_loader("site_list.txt");
 	var blind_run = new array();
 	var cache_run = new array();
-	for var index in loader.url_list{
+	for (var index in loader.url_list){
 		var run_request = new ajaxRequest();
 		var start_time = new Date(milliseconds);
 		run_request.open("GET", loader.url_list[index].url, false);
@@ -52,7 +52,7 @@ document.onReady = function(){
 		var difference = end_time - start_time;
 		blind_run.push(difference);
 	}
-	for var index in loader.url_list{
+	for (var index in loader.url_list){
 		var run_request = new ajaxRequest();
 		var start_time = new Date(milliseconds);
 		run_request.open("GET", loader.url_list[index].url, false);
@@ -61,7 +61,7 @@ document.onReady = function(){
 		var difference = end_time - start_time;
 		cache_run.push(difference);
 	}
-	for var index in blind_run{
+	for (var index in blind_run){
 		document.writeln(blind_run[index]-cache_run[index]);
 	}
 }
