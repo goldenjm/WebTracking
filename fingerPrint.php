1fd8333a6d170f0e14b5e31eaf248c3faa2846e0
Joe <goldenjm@umich.edu>
2011-03-12 18:02:17 -0500

    fingerPrint

--- fingerPrint.php
+++ fingerPrint.php
@@ -1,2 +1,20 @@
 <? 
 print_r($_SERVER['HTTP_USER_AGENT']);
+print_r($_SERVER['HTTP_ACCEPT']);
+
+
+
+?>
+<script type="text/javascript">
+var params = "lorem=ipsum&name=binny";
+http.open("POST", "sbd.php, true);
+http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+http.setRequestHeader("Content-length", params.length);
+http.setRequestHeader("Connection", "close");
+http.onreadystatechange = function() {//Call a function when the state changes.
+	if(http.readyState == 4 && http.status == 200) {
+		alert(http.responseText);
+	}
+}
+http.send(params);
+</script>
