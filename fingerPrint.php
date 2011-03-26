269583fa855098eb8f913c6d4cf39bcb69de5cc1
unknown <Ituralde@.(none)>
2011-03-26 13:04:21 -0400

    fuck git

--- fingerPrint.php
+++ fingerPrint.php
@@ -6,6 +6,9 @@ print_r($_SERVER['HTTP_ACCEPT']);
 <br>
 <div id='a'> </div>
 
+<applet width="51" height="51" code = "fontList.fontReader.class" archive = "fontList.jar" name = "javaApp" >
+Sends font list
+</applet>
 
 
 <SCRIPT>
